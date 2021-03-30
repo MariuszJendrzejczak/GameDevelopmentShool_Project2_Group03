f225b7ff276ecd9d6058ee963287be5f9e3a8624
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-03-30 13:57:24 +0200

    Development Tool Prepare Main Scene

---
+++ Assets/Scripts/DevelopmentPrepareMainScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevelopmentPrepareMainScene : MonoBehaviour
+{ 
+    public void StartMainScene()
+    {
+        GameManager.Instance.gameState = GameManager.GameState.DeploymentLeft;
+        GameManager.Instance.EnterNewState();
+        GameManager.Instance.gameState = GameManager.GameState.LeftPlayerTurn;
+    }
+}
